06c915d960d1e77271f61a2f9455c710bf707760
박승은 <noqo23@naver.com>
2022-12-11 22:00:58 +0900

    junit

---
+++ src/main/java/com/zx/xc/XcApplication.java
@@ -0,0 +1,13 @@
+package com.zx.xc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zx/xc/XcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zx.xc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
